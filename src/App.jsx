944a5c5d664c0593858d628ce79517e4ce654d83
Peter <dayr88520@gmail.com>
2024-10-04 01:20:14 +0800

    fix: homePage path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import HomePage from "./routes/homePage/homePage";
 import ListPage from "./routes/listPage/listPage";
 import SinglePage from "./routes/singlePage/singlePage";
+import HomePage from "./routes/homePage/homePage";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./routes/Layout/layout";
